9eec5f921bf9b80962bbaa00a28fc21ab6c73bb9
TheMrBo <philipguy7@gmail.com>
2026-09-10 15:50:13 -0500

    changed font size

--- lib/main.dart
+++ lib/main.dart
@@ -105,7 +105,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: .center,
           children: [
             const Text('Philip has pushed the button this many times:', style: TextStyle(
-                fontSize: 50, // Adjust this number to make it bigger
+                fontSize: 25, // Adjust this number to make it bigger
                 fontWeight: FontWeight.bold, // Optional: makes it bold too
               ),
             ),
